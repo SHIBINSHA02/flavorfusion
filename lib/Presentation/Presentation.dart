649e7029ea0066074e5f02452569dca3d7e6872e
Niranjan-Biju <niranjansreyas123@gmail.com>
2025-02-01 16:36:35 +0530

    Image added

--- lib/Presentation/Presentation.dart
+++ lib/Presentation/Presentation.dart
@@ -3,18 +3,22 @@ import 'package:flutter/material.dart';
 class PresentationPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final double screenHeight = MediaQuery.of(context).size.height;
     
     return Scaffold(
       body: CustomScrollView(
         slivers: [
           SliverAppBar(
             backgroundColor: Colors.teal, 
-            expandedHeight: screenHeight/3,
+            expandedHeight: 300,
             pinned: true,
-            title: Text("Chicken Biriyani",
-              style: TextStyle(
-                fontWeight: FontWeight.bold
+            floating: true,
+            flexibleSpace: SafeArea(
+              child: FlexibleSpaceBar(
+                background: Image.network(
+                  "https://www.licious.in/blog/wp-content/uploads/2022/06/chicken-hyderabadi-biryani-01.jpg",
+                  fit: BoxFit.fill,
+                  
+                ),
               ),
             ),
           ),
