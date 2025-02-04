cd9de1a0a07d468748411e9905703af05ee269a1
Niranjan-Biju <niranjansreyas123@gmail.com>
2025-02-05 00:46:29 +0530

    Some minor changes
    1. Applied filters to images
    2. Back button made more visible, etc

--- lib/Presentation/Presentation.dart
+++ lib/Presentation/Presentation.dart
@@ -3,34 +3,80 @@ import 'package:flutter/material.dart';
 class PresentationPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
+      backgroundColor: Colors.black, // Ensures black background behind curves
       body: CustomScrollView(
         slivers: [
           SliverAppBar(
-            backgroundColor: Colors.teal, 
             expandedHeight: 300,
-            pinned: true,
             floating: true,
+            iconTheme: IconThemeData(
+              color: Colors.white,
+              size: 25), // Changes back button color
             flexibleSpace: SafeArea(
               child: FlexibleSpaceBar(
-                background: Image.network(
-                  "https://www.licious.in/blog/wp-content/uploads/2022/06/chicken-hyderabadi-biryani-01.jpg",
-                  fit: BoxFit.fill,
-                  
+                background: Stack(
+                  fit: StackFit.expand,
+                  children: [
+                    ColorFiltered(
+                      colorFilter: ColorFilter.mode(
+                        Colors.black26, // Darkens the image
+                        BlendMode.darken,
+                      ),
+                      child: Image.network(
+                        "https://www.cubesnjuliennes.com/wp-content/uploads/2020/07/Chicken-Biryani-Recipe.jpg",
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ),
           ),
           SliverList(
-            delegate: SliverChildListDelegate(
-            [SizedBox(height: 200),
+            delegate: SliverChildListDelegate([
+              Stack(
+                children: [
+                  // Black background to fill the curved area
+                  Container(
+                    height: 50, // Matches curve effect
+                    decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(20),
+                        topRight: Radius.circular(20),
+                      ),
+                    ),
+                  ),
+                  // Food name container
+                  Container(
+                    padding: EdgeInsets.all(16),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(25),
+                        topRight: Radius.circular(25),
+                      ),
+                    ),
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        "Hyderabadi Biryani",
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black87,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
               Container(height: 400, color: Colors.red),
               Container(height: 400, color: Colors.green),
               Container(height: 400, color: Colors.blue),
-        ],
-      ),
-    ),
+            ]),
+          ),
         ],
       ),
     );
